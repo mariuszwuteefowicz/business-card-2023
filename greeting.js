7eec70f75af35728376755e6fa960148d6630597
Mariusz <mariusz.c008@protonmail.com>
2023-09-07 20:35:55 +0200

    homework6

--- greeting.js
+++
@@ -1,8 +0,0 @@
-const greet = (name, age) => {
-    const result = `Witaj ${name}! Masz ${age} lat!`;
-return     result;
-}
-
-const greeting = greet('Maciek', 35);
-console.log(greeting)
-console.log(greet('Karolina', 31))
